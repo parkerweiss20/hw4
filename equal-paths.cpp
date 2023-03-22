a92e082cbbfe1421fd733f9849a5a4d43d3376a5
Parker Weiss <pjweiss@usc.edu>
2023-03-22 04:15:02 +0000

    finished equal paths

---
+++ equal-paths.cpp
@@ -0,0 +1,38 @@
+#ifndef RECCHECK
+//if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <vector>
+#include <set>
+#endif
+
+#include "equal-paths.h"
+
+using namespace std;
+
+
+// You may add any prototypes of helper functions here
+void dfs(Node* node, unsigned int depth, vector<unsigned int>& depths) {
+    if (!node->left && !node->right) {
+        depths.push_back(depth);
+    }
+    if (node->right) {
+        dfs(node->right, depth+1, depths);
+    }
+    if (node->left) {
+        dfs(node->left, depth+1, depths);
+    }
+}
+
+bool equalPaths(Node * root)
+{
+    // Add your code below
+    if(!root){
+      return true;
+    }
+
+    vector<unsigned int> depths;
+
+    dfs(root, 0, depths);
+
+    return (set<unsigned int>(depths.begin(), depths.end()).size() == 1);
+}
+
